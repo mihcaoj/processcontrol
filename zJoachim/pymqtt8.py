6b83b5b412a172b17ea49bca2821a03110070be1
Joachim Bono <joachim.bono@protonmail.com>
2023-12-15 18:00:58 +0100

    added comments

--- zJoachim/pymqtt8.py
+++ zJoachim/pymqtt8.py
@@ -6,33 +6,36 @@ import tkinter as tk
 import atexit
 
 # TODO:
-#        - disconnect / cleanup
+#        - disconnect / cleanup (make sure it works)
 #        - add threads
-#        - track the tracks controller (final project part 0.2.3) --- MAKE SURE IT WORKS
 #        - improve gui
 #        - change lane right and left values need to be modified to work
 #        - fix the slider for acceleration and velocity (values in between still seem to be published)
 
 ip_address = '192.168.4.1'  # ip address of the hyperdrive
 port = 1883  # port for MQTT
-client = mqtt.Client('hyperdrive')
+client = mqtt.Client('hyperdrive') # create an instance of the MQTT client with hyperdrive as the client id
 vehicleID = 'cec233dec1cb'  # change according to the vehicle ID
 
 emergency_topic = "Anki/Emergency/U"  # path for emergency topic
 emergency_flag = False  # setting initial flag value to False
 
-# Declare velocity_slider and acceleration_slider as global variables
+# Declare velocity_slider, acceleration_slider and sliders_updated as global variables
 velocity_slider = None
 acceleration_slider = None
 sliders_updated = False
 
+# Labels for battery, tracks and direction
 battery_label = None
 battery_level = 0
+
 track_label = None
 current_track_id = 0
+
 direction_label = None
 current_direction = 0
-LOW_BATTERY_THRESHOLD = 20
+
+LOW_BATTERY_THRESHOLD = 20 # threshold at which the low battery popup will appear
 
 tkinter_thread = None
 
@@ -55,43 +58,57 @@ payload_off = {
 
 
 def on_connect(client, userdata, flags, rc):
+    # Callback function called when the client successfully connects to the broker
     print(f"Connected with result code {rc}")
 
     payload_discover = {"type": "discover", "payload": {"value": True}}
 
+    # Publish the discover payload to the specified topic
     publish(client, "Anki/Hosts/U/hyperdrive/I/", payload_discover)
     print("Published the discover payload successfully")
 
+    # Check if the connection result code is 0, indicating a successful connection
     if rc == 0:
         print(f"Connected to broker at {ip_address}:{port}")
+
+        # Publish a status message indicating that the vehicle is connected
         publish(client, "Anki/Vehicles/U/" + vehicleID + "/S/status", {"value": "connected"})
         print("Published the connecting payload successfully")
 
 
 def on_message(client, userdata, msg):
+    # Callback function called when a message is received from the broker
     print(f"Received {msg.payload} from {msg.topic}")
+
+    # Parse the JSON payload from the message
     payload = json.loads(msg.payload.decode("utf-8"))
 
     global emergency_flag, battery_level, current_track_id, current_direction
 
+     # Check the topic of the received message
     if msg.topic == emergency_topic:
+        # Update the emergency flag based on the received payload
         emergency_flag = payload.get("value", False)
         print(f"Emergency flag is set to {emergency_flag}")
     elif msg.topic == "Anki/Vehicles/U/" + vehicleID + "/S/battery":
+        # Update battery level based on the received payload
         battery_value = payload.get("value", 0)
         print(f"Battery value is {battery_value}")
         battery_level = battery_value
 
+        # Show a low battery popup if the battery level is below the threshold
         if battery_value < LOW_BATTERY_THRESHOLD:
             show_low_battery_popup()
     elif msg.topic == "Anki/Vehicles/U/" + vehicleID + "/E/track":
+        # Update current track information based on the received payload
         track_id = payload.get("trackId", None)
-        if track_id is not None:  # Check if track_id is not None
+        if track_id is not None: 
             print(f"Received track information. Track ID: {track_id}")
             current_track_id = track_id
         else:
             print("Received track information, but track_id is None.")
 
+        # Update current direction information based on the received payload
         direction = payload.get("direction", "unknown")
         print(f"Received track information. Direction: {direction}")
         current_direction = direction
@@ -195,7 +212,6 @@ def change_lane_left():
 
 
 def stop_vehicle():
-    # sets the pause event to pause the drive_car and change_lane threads
     print("Stopping vehicle: " + vehicleID)
     # If emergency_flag is True, set velocity and acceleration to stop the car
     velocity = 0
@@ -211,7 +227,7 @@ def stop_vehicle():
 
 def change_flag_status():
     global emergency_flag
-    emergency_flag = not emergency_flag
+    emergency_flag = not emergency_flag # change the status of the flag
     print(f"Emergency flag status changed to: {emergency_flag}")
 
 
@@ -251,7 +267,7 @@ def update_velocity_slider(value):
 
         sliders_updated = True
 
-        # Send updated velocity value to the broker
+        # Send the updated velocity value to the broker
         payload_velocity = {
             "type": "speed",
             "payload": {
@@ -267,6 +283,7 @@ def update_acceleration_slider(value):
     if acceleration_slider:
         sliders_updated = True
 
+        # Send the updated acceleration value to the broker
         payload_acceleration = {
             "type": "speed",
             "payload": {
@@ -291,7 +308,7 @@ def update_battery_label():
 def update_track_label():
     # Update the track label
     track_label.config(text=f"Current Track: {current_track_id}")
-    # Schedule the next update after 1 second
+    # Schedule the next update after 1s
     track_label.after(1000, update_track_label)
 
 def update_direction_label():
@@ -310,11 +327,11 @@ def update_gui():
 def run_tkinter():
     def create_tkinter_window():
         global velocity_slider, acceleration_slider, battery_label, track_label, direction_label
-        # Create the main application window
+        # Main application window
         app = tk.Toplevel()
         app.title("Emergency Flag Controller")
 
-        # Create a button to toggle the emergency flag
+        # Button to toggle the emergency flag
         button = tk.Button(app, text="Toggle Emergency Flag", command=change_flag_status)
         button.pack(pady=10)
 
@@ -376,7 +393,8 @@ def run_tkinter():
         # Initial GUI update
         update_gui()
 
-        app.mainloop()  # Run the Tkinter event loop
+        # Run the tkinter event loop
+        app.mainloop() 
 
     def start_tkinter():
         global tkinter_thread
@@ -392,19 +410,17 @@ def run_tkinter():
     # Keep a reference to the app so that it doesn't get garbage collected
     return tkinter_thread
 
-# Add a clean-up function
-def cleanup():
+def cleanup(): ### NOT TESTED YET ###
     client.disconnect()
     pause_drive_event.set()
     pause_lane_event.set()
     emergency_thread.join()
     tkinter_thread.join()
 
-# Call cleanup when the script exits
-atexit.register(cleanup)
 
+atexit.register(cleanup) # call cleanup when the script exits ### NOT TESTED YET ### 
 
-# publish the initial connect message
+# Publish the initial connect message
 json_payload = {
     "type": "connect",
     "payload": {
@@ -412,7 +428,10 @@ json_payload = {
     }
 }
 
+# Print a message indicating the intention to connect to the MQTT broker
 print(f"Connecting to broker at {ip_address}:{port}")
+
+# Establish a connection to the MQTT broker
 client.connect(ip_address, port=port)
 
 client.loop_start()  # start the loop
@@ -420,14 +439,14 @@ subscribe(client)  # subscribe to the client
 publish(client, "Anki/Vehicles/U/" + vehicleID + "/I/", json_payload)  # publish the json payload
 time.sleep(5)
 
-# event objects to control the pause/resume of threads
+# Event objects to control the pause/resume of threads
 pause_drive_event = threading.Event()
 pause_lane_event = threading.Event()
 
-# creation of the different threads
+# Creation of the different threads
 emergency_thread = threading.Thread(target=emergency_stop_process)
 emergency_thread.start()
 
 run_tkinter()
 
-client.loop_stop()
+client.loop_stop()  # stop the MQTT client loop when your script exits
