f86b7dccb2c0e9bf76fb9c94e1c13606167a3dcc
Joachim Bono <joachim.bono@protonmail.com>
2023-12-20 09:17:03 +0100

    added final project version

--- Java/src/mqtt_blinking_prof_code/weakcoupling/MessageListener.java
+++
@@ -1,8 +0,0 @@
-package mqtt_blinking_prof_code.weakcoupling;
-
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
-public interface MessageListener {
-    public void addMQTTMessage(String topic, MqttMessage message);
-    
-}
